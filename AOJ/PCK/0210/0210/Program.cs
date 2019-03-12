029eb55dec46c3fecb66e4c4550feb34ad4bb028
Kyosuke Nakamura <n_kyoro@hotmail.com>
2019-03-13 01:48:27 +0900

    Add:0210,0211

---
+++ AOJ/PCK/0210/0210/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _0210
+{
+    public class Program
+
+    {
+        private const char BLANK = '.';
+        private const char WALL = '#';
+        private const char EXIT = 'X';
+        private const int TIME_LIMIT = 180;
+
+        private enum Direction { W, S, E, N, Cnt }
+
+        private class Point
+        {
+            public int I { get; set; }
+            public int J { get; set; }
+            public Point(int i, int j) { I = i; J = j; }
+        }
+
+        private class Person
+        {
+            public Point Pos { get; set; }
+            public Direction Dir { get; set; }
+            public Point NxtPos { get; set; }
+            public Direction NxtDir { get; set; }
+            public Person(int i, int j, Direction d) { Pos = new Point(i, j); Dir = d; }
+        }
+
+        private static readonly Point[] dPos = new Point[] { new Point(0, -1), new Point(1, 0), new Point(0, 1), new Point(-1, 0) };
+        private static char[,] map;
+        private static List<Person> psns;
+
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                int[] wh = RIntAr();
+                if (wh.Sum() == 0) break;
+                Init(wh);
+                Console.WriteLine(CalcExitTime());
+            }
+        }
+
+        private static void Init(int[] wh)
+        {
+            map = new char[wh[1], wh[0]];
+            psns = new List<Person>();
+
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                string line = RSt();
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    char c = line[j];
+                    map[i, j] = c;
+                    if (c != BLANK && c != WALL && c != EXIT)
+                    {
+                        psns.Add(new Person(i, j, (Direction)Enum.Parse(typeof(Direction), c.ToString())));
+                    }
+                }
+            }
+        }
+
+        private static string CalcExitTime()
+        {
+            int time = 0;
+            while (time <= TIME_LIMIT)
+            {
+                time++;
+                CalcNxt();
+                Move();
+                if (psns.Count == 0) return time.ToString();
+            }
+            return "NA";
+        }
+
+        private static void CalcNxt()
+        {
+            const int INIT = -1;
+            int[,] conflictIDMap = new int[map.GetLength(0), map.GetLength(1)];
+            for (int i = 0; i < conflictIDMap.GetLength(0); i++)
+            {
+                for (int j = 0; j < conflictIDMap.GetLength(1); j++)
+                {
+                    conflictIDMap[i, j] = INIT;
+                }
+            }
+
+            for (int i = 0; i < psns.Count(); i++)
+            {
+                psns[i].NxtPos = psns[i].Pos;
+                for (int j = 0; j < (int)Direction.Cnt; j++)
+                {
+                    Direction nxtDir = (Direction)(((int)psns[i].Dir + ((int)Direction.Cnt - 1) + j) % (int)Direction.Cnt);
+                    int nxtI = psns[i].Pos.I + dPos[(int)nxtDir].I;
+                    int nxtJ = psns[i].Pos.J + dPos[(int)nxtDir].J;
+
+                    if (map[nxtI, nxtJ] == BLANK || map[nxtI, nxtJ] == EXIT)
+                    {
+                        psns[i].NxtDir = nxtDir;
+
+                        int conflictID = conflictIDMap[nxtI, nxtJ];
+                        if (conflictID == INIT || (int)nxtDir < (int)psns[conflictID].NxtDir)
+                        {
+                            psns[i].NxtPos = new Point(nxtI, nxtJ);
+                            conflictIDMap[nxtI, nxtJ] = i;
+                            if (conflictID != INIT) psns[conflictID].NxtPos = psns[conflictID].Pos;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        private static void Move()
+        {
+            for (int i = psns.Count() - 1; i >= 0; i--)
+            {
+                map[psns[i].Pos.I, psns[i].Pos.J] = BLANK;
+
+                psns[i].Dir = psns[i].NxtDir;
+                psns[i].Pos = psns[i].NxtPos;
+
+                if (map[psns[i].Pos.I, psns[i].Pos.J] == EXIT)
+                {
+                    psns.RemoveAt(i);
+                }
+                else
+                {
+                    map[psns[i].Pos.I, psns[i].Pos.J] = Enum.GetName(typeof(Direction), psns[i].Dir)[0];
+                }
+            }
+        }
+
+        static string RSt() { return Console.ReadLine(); }
+        static int RInt() { return int.Parse(Console.ReadLine().Trim()); }
+        static long RLong() { return long.Parse(Console.ReadLine().Trim()); }
+        static double RDouble() { return double.Parse(Console.ReadLine()); }
+        static string[] RStAr(char sep = ' ') { return Console.ReadLine().Trim().Split(sep); }
+        static int[] RIntAr(char sep = ' ') { return Array.ConvertAll(Console.ReadLine().Trim().Split(sep), e => int.Parse(e)); }
+        static long[] RLongAr(char sep = ' ') { return Array.ConvertAll(Console.ReadLine().Trim().Split(sep), e => long.Parse(e)); }
+        static double[] RDoubleAr(char sep = ' ') { return Array.ConvertAll(Console.ReadLine().Trim().Split(sep), e => double.Parse(e)); }
+        static string WAr<T>(IEnumerable<T> array, string sep = " ") { return string.Join(sep, array.Select(x => x.ToString()).ToArray()); }
+    }
+
+}
---
+++ AOJ/PCK/0211/0211/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _0211
+{
+    public class Program
+
+    {
+        public static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                int n = RInt();
+                if (n == 0) break;
+
+                long[] d = new long[n];
+                long[] v = new long[n];
+
+                long lcm = 0;
+                long gcd = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    long[] items = RLongAr();
+                    long g = Gcd(items[0], items[1]);
+                    d[i] = items[0] / g;
+                    v[i] = items[1] / g;
+
+                    if (i == 0)
+                    { 
+                        lcm = d[i]; gcd = v[i];
+                    }
+                    else
+                    {
+                        lcm = Lcm(lcm, d[i]); gcd = Gcd(gcd, v[i]);
+                    }
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine(lcm / d[i] * v[i] / gcd);
+                }
+
+            }
+        }
+
+        /// <summary>
+        ///最小公倍数
+        /// </summary>
+        public static long Lcm(long a, long b)
+        {
+            return a / Gcd(a, b) * b;
+        }
+
+        /// <summary>
+        ///最大公約数
+        /// </summary>
+        public static long Gcd(long a, long b)
+        {
+            if (a < b) return Gcd(b, a);  // 引数を入替えて自分を呼び出す
+            if (b == 0) return a; //一方が0の場合は、もう片方の数自身がGCD
+
+            long d = 0;
+            do
+            {
+                d = a % b;
+                a = b;
+                b = d;
+            } while (d != 0);
+            return a;
+        }
+
+        static string RSt() { return Console.ReadLine(); }
+        static int RInt() { return int.Parse(Console.ReadLine().Trim()); }
+        static long RLong() { return long.Parse(Console.ReadLine().Trim()); }
+        static double RDouble() { return double.Parse(Console.ReadLine()); }
+        static string[] RStAr(char sep = ' ') { return Console.ReadLine().Trim().Split(sep); }
+        static int[] RIntAr(char sep = ' ') { return Array.ConvertAll(Console.ReadLine().Trim().Split(sep), e => int.Parse(e)); }
+        static long[] RLongAr(char sep = ' ') { return Array.ConvertAll(Console.ReadLine().Trim().Split(sep), e => long.Parse(e)); }
+        static double[] RDoubleAr(char sep = ' ') { return Array.ConvertAll(Console.ReadLine().Trim().Split(sep), e => double.Parse(e)); }
+        static string WAr<T>(IEnumerable<T> array, string sep = " ") { return string.Join(sep, array.Select(x => x.ToString()).ToArray()); }
+    }
+
+}
