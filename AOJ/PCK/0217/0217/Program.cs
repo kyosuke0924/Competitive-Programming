d8af5beebbb1f054fb4d2a839f6d81f1227d110e
Kyosuke Nakamura <n_kyoro@hotmail.com>
2019-04-04 02:13:53 +0900

    add

---
+++ AOJ/PCK/0217/0217/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _0217
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                int n = RInt();
+                if (n == 0) break;
+
+                Dictionary<int, int> patients = new Dictionary<int, int>(n);
+                for (int i = 0; i < n; i++)
+                {
+                    int[] items = RIntAr();
+                    patients.Add(items[0], items[1] + items[2]);
+                }
+
+                var item = patients.OrderByDescending(x => x.Value).First();
+                Console.WriteLine("{0} {1}", item.Key, item.Value);
+            }
+
+        }
+
+        static string RSt() { return Console.ReadLine(); }
+        static int RInt() { return int.Parse(Console.ReadLine().Trim()); }
+        static long RLong() { return long.Parse(Console.ReadLine().Trim()); }
+        static double RDouble() { return double.Parse(Console.ReadLine()); }
+        static string[] RStAr(char sep = ' ') { return Console.ReadLine().Trim().Split(sep); }
+        static int[] RIntAr(char sep = ' ') { return Array.ConvertAll(Console.ReadLine().Trim().Split(sep), e => int.Parse(e)); }
+        static long[] RLongAr(char sep = ' ') { return Array.ConvertAll(Console.ReadLine().Trim().Split(sep), e => long.Parse(e)); }
+        static double[] RDoubleAr(char sep = ' ') { return Array.ConvertAll(Console.ReadLine().Trim().Split(sep), e => double.Parse(e)); }
+        static string WAr<T>(IEnumerable<T> array, string sep = " ") { return string.Join(sep, array.Select(x => x.ToString()).ToArray()); }
+    }
+}
